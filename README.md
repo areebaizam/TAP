04ed4602f5d5e9ca26c7044fc2bacabe4e43e5ee
Areeb <areeebaizam@gmail.com>
2022-10-31 23:22:45 -0700

    Add PWA

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ADD SSR
 ng add @nguniversal/express-engine
+npm run build:ssr
+npm run dev:ssr
+
+# ADD PWA
+ng add @angular/pwa
+ng build
+http-server -p 8080 -c-1 dist/TAP/browser
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { APP_ROUTES } from './routes';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     RouterModule.forRoot(APP_ROUTES),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerImmediately'
+    }),
   ],
   bootstrap: [AppComponent]
 })
